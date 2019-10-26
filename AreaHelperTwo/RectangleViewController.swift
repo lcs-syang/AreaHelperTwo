c0b711002336de99ee1eee5a40643d866a99ba5d
Selena Yang <syang@lcs.on.ca>
2019-10-26 15:13:40 -0400

    added triangleViewController file and connect the view controller to the file

---
+++ AreaHelperTwo/RectangleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelperTwo
+//
+//  Created by Yang, Selena on 2019/10/25.
+//  Copyright © 2019 Yang Selena. All rights reserved.
+//
+
+import UIKit
+
+class RectangleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
