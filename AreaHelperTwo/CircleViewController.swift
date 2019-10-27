498d7d95ce9593033c0f3158fcc47b155d0c47e1
Selena Yang <syang@lcs.on.ca>
2019-10-26 23:58:43 -0400

    added outlets and code to run the circle view controller

--- AreaHelperTwo/CircleViewController.swift
+++ AreaHelperTwo/CircleViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class CircleViewController: UIViewController {
     
+   
+    @IBOutlet weak var radiusInput: UITextField!
+    @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func areaOfCircle(_ sender: Any) {
+        guard let radius = radiusInput.text else {
+            result.text = "Please enter a value for radius"
+            return
+        }
+        guard let radiusNumber = Double(radius) else { return
+            result.text = "Please enter a number for radius"
+        }
+        
+        let pi = 3.1415926535
+        let area = pi * radiusNumber * radiusNumber
+        let stringArea = String(area)
+        result.text = stringArea
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
 }
 
+}
