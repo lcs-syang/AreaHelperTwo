4c1415189e1bdbd8d81ca1a42bf1d750166b9b02
Selena Yang <syang@lcs.on.ca>
2019-10-27 00:21:20 -0400

    added outlets and code to run rectangle area

--- AreaHelperTwo/RectangleViewController.swift
+++ AreaHelperTwo/RectangleViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class RectangleViewController: UIViewController {
     
+    @IBOutlet weak var lengthInput: UITextField!
+    @IBOutlet weak var widthInput: UITextField!
+    @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func areaOfRectangle(_ sender: Any) {
+        guard let length = lengthInput.text else {
+            result.text = "Please enter a value for length"
+            return
+        }
+        guard let width = widthInput.text else {
+            result.text = "Please enter a value for width"
+            return
+        }
+        guard let lengthNumber = Double(length) else { return
+            result.text = "Please enter a number for length"
+            
+        }
+        guard let widthNumber = Double(width) else { return
+            result.text = "Please enter a number for width"
+            
+        }
+        
+        let area = widthNumber * lengthNumber
+        let stringArea = String(area)
+        result.text = stringArea
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)    
 }
 
+}
