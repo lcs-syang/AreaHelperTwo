c4b499de8c68c7e3654239acff7237aaee1862a9
Selena Yang <syang@lcs.on.ca>
2019-10-26 23:44:49 -0400

    added outlets to the triangle view controller

--- AreaHelperTwo/TriangleViewController.swift
+++ AreaHelperTwo/TriangleViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class TriangleViewController: UIViewController {
     
+    @IBOutlet weak var baseInput: UITextField!
+    @IBOutlet weak var heightInput: UITextField!
+    @IBOutlet weak var result: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func areaOfTriangle(_ sender: Any) {
+        guard let base = baseInput.text else{
+            result.text = "Plase enter a value for base"
+            return
+        }
+        guard let height = heightInput.text else {
+            result.text = "Please enter a value for height"
+            return
+        }
+        guard let baseNumber = Double(base) else { return
+        result.text = "Please enter a number for base"
+        }
+        guard let heightNumber = Double(height) else { return
+            result.text = "Please enter a number for height"
+        }
+        
+        let area = baseNumber * heightNumber / 2
+        let stringArea = String(area)
+        result.text = stringArea
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
     
 }
 
